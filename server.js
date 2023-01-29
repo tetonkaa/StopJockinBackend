d44692ece896b3e1f3a37ea6c3155c4ee7c6f673
Angel <angel.l.puente@gmail.com>
2023-01-28 20:42:23 -0600

    update

--- server.js
+++ server.js
@@ -15,14 +15,6 @@ app.use(express.json())
 app.use(passport.initialize())
 
 
-//Cors solution
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-  });
 
 // Redirect root to Admin panel
 app.get('/', (_, res) => {
