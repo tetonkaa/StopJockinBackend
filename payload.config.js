aed1f5a2a296ba3f2fbbb5d3df45febb77b4abbb
Angel <angel.l.puente@gmail.com>
2023-02-24 19:55:02 -0600

    update payload admin load

--- payload.config.js
+++ payload.config.js
@@ -5,7 +5,7 @@ import Tags from './collections/Tags';
 import Users from './collections/Users';
 
 export default buildConfig({
-  serverURL: 'http://jockloc.herokuapp.com',
+  serverURL: 'http://localhost:5000',
   admin: {
     user: Users.slug,
   },
